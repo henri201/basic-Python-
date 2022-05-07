9ac2339f684619d221a75f0c59f7fc24ac251f74
henri201 <64684431+henri201@users.noreply.github.com>
2022-05-07 16:53:10 +0300

    Update python.py

--- python.py
+++ python.py
@@ -4,3 +4,27 @@ BASIC MATH OPERATORS :
 7 % 3 (1) returns whats left after division
 2**2  (4) squared
 _  to use the last sum
+
+
+x = ("apple", "banana", "cherry")  tuple
+x = ["apple", "banana", "cherry"] list
+x = {"name" : "John", "age" : 36} dict
+type()  #to check for type
+
+"mr gregs house \n is there" # second part goes to other line    
+
+\t # is tab in a string
+
+len() #returns the length of the string
+
+.index() # returns the index of what you are lookign for
+
+.count()  # how many times it appears
+
+slicing a string [start index : end index] # does stops before the end   ## you can also have [4:] from 4th to the end, [:4] from 0 to 4th
+
+'string you want to check' in variableName  #check if a variable is in a string  ## can also use   not in
+
+.upper() #to upper case a string .lower()
+
+print(f"the sum of {x} and {y} is {sum}")  # f formating , like ${} in javascript
